dbdd084992e41d553aab220c4dd52f40fb47b26a
Chaitanya Reddy <chaitanyareddygujjula@gmail.com>
2024-03-23 15:39:12 +0530

    changed working of shimmer in home page

--- src/components/Shimmer.js
+++ src/components/Shimmer.js
@@ -1,10 +1,22 @@
 const Shimmer = () =>{
     return(
-        <div className="shimmer">
-            <div className="shimmer-container"> </div>
-            <div className="shimmer-container"> </div>
-            <div className="shimmer-container"> </div>
-            <div className="shimmer-container"> </div>
+        <div>
+            <div className="my-8 flex flex-wrap justify-left ">
+                <button className="mx-4 my-4 bg-[#bdbdbd] w-36 h-12 rounded-3xl hover:shadow-xl hover:cursor-pointer hover:bg-[#969595]"></button>
+                <button className="mx-4 my-4 bg-[#bdbdbd] w-36 h-12 rounded-3xl hover:shadow-xl hover:cursor-pointer hover:bg-[#969595]"></button>
+                <button className="mx-4 my-4 bg-[#bdbdbd] w-36 h-12 rounded-3xl hover:shadow-xl hover:cursor-pointer hover:bg-[#969595]"></button>
+                <button className="mx-4 my-4 bg-[#bdbdbd] w-36 h-12 rounded-3xl hover:shadow-xl hover:cursor-pointer hover:bg-[#969595]"></button>
+            </div>
+            <div className="mx-[10%] flex flex-wrap justify-left ">
+                <div className="my-4 mx-7 h-[330px] w-[20%] rounded-xl bg-[#bdbdbd] hover:shadow-xl hover:cursor-pointer hover:bg-[#969595]"> </div>
+                <div className="my-4 mx-7 h-[330px] w-[20%] rounded-xl bg-[#bdbdbd] hover:shadow-xl hover:cursor-pointer hover:bg-[#969595]"> </div>
+                <div className="my-4 mx-7 h-[330px] w-[20%] rounded-xl bg-[#bdbdbd] hover:shadow-xl hover:cursor-pointer hover:bg-[#969595]"> </div>
+                <div className="my-4 mx-7 h-[330px] w-[20%] rounded-xl bg-[#bdbdbd] hover:shadow-xl hover:cursor-pointer hover:bg-[#969595]"> </div>
+                <div className="my-4 mx-7 h-[330px] w-[20%] rounded-xl bg-[#bdbdbd] hover:shadow-xl hover:cursor-pointer hover:bg-[#969595]"> </div>
+                <div className="my-4 mx-7 h-[330px] w-[20%] rounded-xl bg-[#bdbdbd] hover:shadow-xl hover:cursor-pointer hover:bg-[#969595]"> </div>
+                <div className="my-4 mx-7 h-[330px] w-[20%] rounded-xl bg-[#bdbdbd] hover:shadow-xl hover:cursor-pointer hover:bg-[#969595]"> </div>
+                <div className="my-4 mx-7 h-[330px] w-[20%] rounded-xl bg-[#bdbdbd] hover:shadow-xl hover:cursor-pointer hover:bg-[#969595]"> </div>
+            </div>
         </div>
     );
 }
