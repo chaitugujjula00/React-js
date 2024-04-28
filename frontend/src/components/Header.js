249b8b2ad1793702ad4a5b5c5c04dc0e3e93aa03
Chaitanya Reddy <chaitanyareddygujjula@gmail.com>
2024-04-28 23:15:19 +0530

    drop down for account

--- frontend/src/components/Header.js
+++ frontend/src/components/Header.js
@@ -35,7 +35,19 @@ const remove =()=>{
           { user === null ? (<li className="m-4 hover:cursor-pointer">
             <Link to="/login">Login</Link>
           </li>):(<li className="m-4 hover:cursor-pointer">
-            <div onClick={remove}><Link to="/">{user.userName}</Link></div>
+          <div class="dropdown text-gray-700">
+        <div class="flex items-center cursor-pointer">
+            <span class="mr-1">{user.userName}</span>
+            <svg xmlns="http://www.w3.org/2000/svg" class="h-5 w-5 fill-current" viewBox="0 0 20 20" fill="currentColor">
+                <path fill-rule="evenodd" d="M10 12a1 1 0 0 1-.707-.293l-4-4a1 1 0 0 1 1.414-1.414L10 10.586l3.293-3.293a1 1 0 1 1 1.414 1.414l-4 4A1 1 0 0 1 10 12z" clip-rule="evenodd" />
+            </svg>
+        </div>
+        <div class="dropdown-menu bg-white border rounded shadow-md p-2 w-32">
+            <a class="block px-4 py-2 text-gray-800 hover:bg-gray-200">Account</a>
+            <a class="block px-4 py-2 text-gray-800 hover:bg-gray-200">Orders</a>
+            <a class="block px-4 py-2 text-gray-800 hover:bg-gray-200" onClick={remove}>Sign Out</a>
+        </div>
+    </div>
           </li>)}
         </ul>
       </div>
